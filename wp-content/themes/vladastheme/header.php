81983705bd3849949c5a9027c33c39325c4e9356
vladasto <1vladastojanovic1@gmail.com>
2024-09-24 23:46:06 +0200

    Lighthouse improvements

--- wp-content/themes/vladastheme/header.php
+++ wp-content/themes/vladastheme/header.php
@@ -1,11 +1,11 @@
 
 <!DOCTYPE html>
-    <html>
+<html <?php language_attributes(); ?>>
     <body <?php body_class(); ?>>
 
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
 
 
--- wp-content/themes/vladastheme/inc/_partials/nav.php
+++ wp-content/themes/vladastheme/inc/_partials/nav.php
@@ -7,7 +7,7 @@ $site_logo = get_field('site_logo', 'option');
         <div class="m-nav__content">
             <div class="m-nav__content--logo">
                 <a href="/">
-                    <img src="<?php echo $site_logo; ?>" alt="">
+                    <img src="<?php echo $site_logo; ?>" alt="expeditor logo">
                 </a>
             </div>
 
